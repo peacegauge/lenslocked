05a86b8432bee925578e6697e339416e0e07d1f7
ezraorbit <emakankwasa@alu.ucam.edu>
2024-10-18 08:46:01 +0200

    Mae the FAQ page use dynamically provided questions

--- controllers/static.go
+++ controllers/static.go
@@ -11,3 +11,26 @@ func StaticHandler(tpl views.Template) http.HandlerFunc {
 		tpl.Execute(w, nil)
 	}
 }
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{{
+		Question: "Is there a free version?",
+		Answer:   "Yes! We offer a  free trial for 30 days",
+	},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   "Email us - support@lenslocked.com",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
