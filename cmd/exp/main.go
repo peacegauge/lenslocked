512ddfa0a2c7ab5a2f3ea5b1b1a20cd9f020bd49
ezraorbit <emakankwasa@alu.ucam.edu>
2024-10-21 13:27:27 +0200

    Instruction set to insert data into the database

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -41,4 +41,36 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Connected")
+
+	//Create a table....
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		email TEXT UNIQUE NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS orders (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		amount INT,
+		description TEXT
+	);
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables created")
+
+	//Insert some data
+	name := "Jon Bandit"
+	email := "test@email.com"
+	_, err = db.Exec(`
+	INSERT INTO users (name, email)
+	VALUES ($1, $2);`, name, email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User created.")
 }
